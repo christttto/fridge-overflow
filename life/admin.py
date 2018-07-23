14f6a5d264eab670aff74ff3a68c2b3e55ca4157
christttto <christttto@gmail.com>
2018-07-23 05:14:22 -0400

    jake stuff

--- life/admin.py
+++ life/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from life.models import *
 
 # Register your models here.
-admin.site.register(Group)
-admin.site.register(Question)
-admin.site.register(Activity)
+# admin.site.register(Group)
+
+# My code
+admin.site.register(User)
 admin.site.register(Item)
--- life/models.py
+++ life/models.py
@@ -5,8 +5,17 @@ class Group(models.Model):
 	name = models.CharField(max_length=50)
 
 class Question(models.Model):
-   question_text = models.CharField(max_length=200)
-   pub_date = models.DateTimeField('date published')
+  question_text = models.CharField(max_length=200)
+  pub_date = models.DateTimeField('date published')
+  def __str__(self):
+    return self.question_text
+  
+class Choice(models.Model):
+  question = models.ForeignKey(Question,on_delete=models.CASCADE)
+  choice_text = models.CharField(max_length=200)
+  votes = models.IntegerField(default=0)
+  def __str__(self):
+    return self.choice_text
   
 class Activity(models.Model):
   name = models.CharField(max_length=50)
@@ -21,4 +30,3 @@ class Item(models.Model):
   stock = models.BooleanField(default=False)
   #purchased_date
   #purchased-by
-  
